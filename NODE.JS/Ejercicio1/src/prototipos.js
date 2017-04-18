e5d5507c2f2439d3958527c66b3f9b6cc01f4d17
blancaromero <blanca.romero@babel.es>
2017-04-18 10:00:03 +0200

    prototipos.js

--- NODE.JS/Ejercicio1/src/prototipos.js
+++ NODE.JS/Ejercicio1/src/prototipos.js
@@ -11,3 +11,31 @@ Persona.prototype.saluda = function() {
 };
 
 persona.saluda();
+
+// ------ HERENCIA
+
+
+function Agente(nombre) {
+    Persona.call(this, nombre);
+    //Esto ejecuta el constructor de Persona con el this de Agente
+}
+
+// asignamos como prototipo una persona
+Agente.prototype = new Persona('soy un prototipo');
+
+const agente = new Agente('Smith');
+
+agente.saluda();
+
+// ------ HERENCIA MULTIPLE
+
+function Superheroe() {
+    this.vuela = function() {
+        console.log(this.nombre, 'vuela');
+    };
+    this.esquivaBalas = function() {
+        console.log(this.nombre, 'esquiva balas');
+    };
+}
+
+// Asignar todas las propiedades (y metodos) de un Superheroe al prototipo de Agente
