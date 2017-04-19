5948465077e78286482245e6c88e254c19ad8dab
blancaromero <blanca.romero@babel.es>
2017-04-19 14:01:22 +0200

    mongodb

---
+++ NODE.JS/MongoDB/ejemplo_mongodb_driver/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/cursonode',
+    function(err, db) {
+        if(err) {
+            console.log('Error', err);
+            process.exit(1);
+        }
+        // REcuperamos documentos de la coleccion agentes
+        db.collection('agentes').find().toArray(function(err, datos) {
+            if(err) {
+                console.log('Error', err);
+                process.exit(1);
+            }
+            datos.forEach(documento => {
+                console.log(documento.name, docuemnto.age);
+            });
+        db.close();
+    });
+    });        
